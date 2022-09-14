ea39503232de001d42dc094e8202c971a456eca5
Sailor Cat <52355344+ActualSailorCat@users.noreply.github.com>
2022-09-14 12:23:04 -0400

    1.03

---
+++ Valkrana 1.03/Core/Sailor_Valkrana_Config.sql
@@ -0,0 +1,31 @@
+--///////////////////////////////////////////////////////
+-- Valkrana / Config
+--///////////////////////////////////////////////////////
+-- // Players
+INSERT INTO Players	(Domain, PortraitBackground, LeaderType, LeaderName, LeaderIcon, LeaderAbilityName, LeaderAbilityDescription, LeaderAbilityIcon, CivilizationType, CivilizationName, CivilizationIcon, CivilizationAbilityName, CivilizationAbilityDescription, CivilizationAbilityIcon)
+SELECT DISTINCT Domain,
+		'LEADER_SAILOR_VALKRANA_BACKGROUND', -- PortraitBackground
+		'LEADER_SAILOR_VALKRANA', -- LeaderType
+		'LOC_LEADER_SAILOR_VALKRANA_NAME', -- LeaderName
+		'ICON_LEADER_SAILOR_VALKRANA', -- LeaderIcon
+		'LOC_TRAIT_LEADER_SAILOR_VALKRANA_UA_NAME', -- LeaderAbilityName
+		'LOC_TRAIT_LEADER_SAILOR_VALKRANA_UA_DESCRIPTION', -- LeaderAbilityDescription
+		'ICON_LEADER_SAILOR_VALKRANA', -- LeaderAbilityIcon
+		CivilizationType, CivilizationName, CivilizationIcon, CivilizationAbilityName, CivilizationAbilityDescription, CivilizationAbilityIcon
+FROM Players WHERE CivilizationType = 'CIVILIZATION_RUSSIA';
+
+-- // PlayerItems
+INSERT INTO PlayerItems	(Domain, LeaderType, CivilizationType, Type, Icon, Name, Description, SortIndex)
+SELECT DISTINCT Domain, 'LEADER_SAILOR_VALKRANA', CivilizationType, Type, Icon, Name, Description, SortIndex
+FROM PlayerItems WHERE CivilizationType = 'CIVILIZATION_RUSSIA';
+
+INSERT INTO PlayerItems	(Domain, LeaderType, CivilizationType, Type, Icon, Name, Description, SortIndex)
+SELECT DISTINCT Domain,
+		'LEADER_SAILOR_VALKRANA',
+		'CIVILIZATION_RUSSIA',
+		'UNIT_SAILOR_VALKRANA_UU',
+		'ICON_UNIT_SAILOR_VALKRANA_UU',
+		'LOC_UNIT_SAILOR_VALKRANA_UU_NAME',
+		'LOC_UNIT_SAILOR_VALKRANA_UU_DESCRIPTION',
+		30
+FROM PlayerItems WHERE CivilizationType = 'CIVILIZATION_RUSSIA';
---
+++ Valkrana 1.03/Core/Sailor_Valkrana_Text.sql
@@ -0,0 +1,36 @@
+-- English
+INSERT OR REPLACE INTO LocalizedText
+        (Tag,    Language,    Text)
+VALUES	("LOC_PEDIA_LEADERS_PAGE_LEADER_SAILOR_VALKRANA_CHAPTER_DETAILED_BODY",
+		"en_US",
+		"SPELLBOOK[NEWLINE][ICON_Bullet][COLOR_FLOAT_CULTURE]Scry[ENDCOLOR] - Permanently reveal a city belonging to target leader. Targets a different city per use, or increases your visibility radius over a city. Targets a random plot if no leaders have been met.[NEWLINE][ICON_Bullet][COLOR_MEDIUM_GREEN]Kinetic Barrier[ENDCOLOR] - Grant your units a +10 Combat Strength bonus when defending. Lasts 5 turns and stacks duration.[NEWLINE][ICON_Bullet][COLOR:72,39,111,255]Well of Souls[ENDCOLOR] - Heal your skeletons for an amount equal to 20 plus twice the number of units you have destroyed since you last used this spell.[NEWLINE][ICON_Bullet][COLOR:72,39,111,255]Skeleport[ENDCOLOR] - Spawn 2 to 3 hostile skeletons near target leader's city. Targets a random plot if no leaders have been met.");
+-- Chinese Traditional
+INSERT OR REPLACE INTO LocalizedText
+        (Tag,    Language,    Text)
+VALUES	("LOC_PEDIA_LEADERS_PAGE_LEADER_SAILOR_VALKRANA_CHAPTER_DETAILED_BODY",
+		"zh_Hant_HK",
+		"拼寫書[NEWLINE][ICON_Bullet][COLOR_FLOAT_CULTURE]占卜[ENDCOLOR] - 永久顯示屬於目標領導人的城市。 每次使用以不同的城市為目標，或增加您在城市上的能見度半徑。 如果沒有遇到領導者，則以隨機情節為目標。[NEWLINE][ICON_Bullet][COLOR_MEDIUM_GREEN]動能屏障[ENDCOLOR] - 防禦時為您的單位提供 +10 戰鬥力加成。 持續 5 回合併堆疊持續時間。[NEWLINE][ICON_Bullet][COLOR:72,39,111,255]靈魂之井[ENDCOLOR] - 治療你的骷髏，治療量等於 20 加上自你上次使用此法術以來摧毀的單位數量的兩倍。[NEWLINE][ICON_Bullet][COLOR:72,39,111,255]骨架端口[ENDCOLOR] - 在目標領導人的城市附近生成 2 到 3 個敵對骷髏。 如果沒有遇到領導者，則以隨機情節為目標。");
+-- Chinese Simplified
+INSERT OR REPLACE INTO LocalizedText
+        (Tag,    Language,    Text)
+VALUES	("LOC_PEDIA_LEADERS_PAGE_LEADER_SAILOR_VALKRANA_CHAPTER_DETAILED_BODY",
+		"zh_Hans_CN",
+		"拼写书[NEWLINE][ICON_Bullet][COLOR_FLOAT_CULTURE]占卜[ENDCOLOR] - 永久显示属于目标领导人的城市。 每次使用以不同的城市为目标，或增加您在城市上的能见度半径。 如果没有遇到领导者，则以随机情节为目标。[NEWLINE][ICON_Bullet][COLOR_MEDIUM_GREEN]动能屏障[ENDCOLOR] - 防御时为您的单位提供 +10 战斗力加成。 持续 5 回合并堆叠持续时间。[NEWLINE][ICON_Bullet][COLOR:72,39,111,255]灵魂之井[ENDCOLOR] - 治疗你的骷髅，治疗量等于 20 加上自你上次使用此法术以来摧毁的单位数量的两倍。[NEWLINE][ICON_Bullet][COLOR:72,39,111,255]骨架端口[ENDCOLOR] - 在目标领导人的城市附近生成 2 到 3 个敌对骷髅。 如果没有遇到领导者，则以随机情节为目标。");
+-- Russian
+INSERT OR REPLACE INTO LocalizedText
+        (Tag,    Language,    Text)
+VALUES	("LOC_PEDIA_LEADERS_PAGE_LEADER_SAILOR_VALKRANA_CHAPTER_DETAILED_BODY",
+		"ru_RU",
+		"СПЕЛЛБУК[NEWLINE][ICON_Bullet][COLOR_FLOAT_CULTURE]Скри[ENDCOLOR] - Постоянно раскрывает город, принадлежащий целевому лидеру. За каждое использование выбирает другой город или увеличивает радиус видимости над городом. Нацеливается на случайный участок, если ни один лидер не был встречен.[NEWLINE][ICON_Bullet][COLOR_MEDIUM_GREEN]Кинетический барьер[ENDCOLOR] - Дает вашим юнитам бонус +10 к боевой силе при защите. Действует 5 ходов и складывается по времени.[NEWLINE][ICON_Bullet][COLOR:72,39,111,255]Колодец душ[ENDCOLOR] - исцеляет ваших скелетов на сумму, равную 20 плюс удвоенное количество единиц, уничтоженных с момента последнего использования этого заклинания.[NEWLINE][ICON_Bullet][COLOR:72,39,111,255]Скелепорт[ENDCOLOR] - порождает от 2 до 3 враждебных скелетов возле города лидера. Нацеливается на случайный участок, если ни один лидер не был встречен.");
+-- German
+INSERT OR REPLACE INTO LocalizedText
+        (Tag,    Language,    Text)
+VALUES	("LOC_PEDIA_LEADERS_PAGE_LEADER_SAILOR_VALKRANA_CHAPTER_DETAILED_BODY",
+		"de_DE",
+		"SPELLBOOK[NEWLINE][ICON_Bullet][COLOR_FLOAT_CULTURE]Hellsehen[ENDCOLOR] - Offenbart dauerhaft eine Stadt, die dem Anführer gehört. Zielt bei jedem Einsatz auf eine andere Stadt oder erhöht den Sichtbarkeitsradius über einer Stadt. Zielt auf ein zufälliges Grundstück, wenn kein Anführer getroffen wurde.[NEWLINE][ICON_Bullet][COLOR_MEDIUM_GREEN]Kinetische Barriere[ENDCOLOR] - Gewährt deinen Einheiten bei der Verteidigung einen Bonus von +10 Kampfstärke. Hält 5 Runden an und ist stapelbar.[NEWLINE][ICON_Bullet][COLOR:72,39,111,255]Brunnen der Seelen[ENDCOLOR] - Heilt deine Skelette für einen Betrag von 20 plus der doppelten Anzahl an Einheiten, die du seit der letzten Verwendung dieses Zaubers zerstört hast.[NEWLINE][ICON_Bullet][COLOR:72,39,111,255]Skeleport[ENDCOLOR] - Bringt 2 bis 3 feindliche Skelette in der Nähe der Stadt des Anführers hervor. Zielt auf ein zufälliges Grundstück, wenn kein Anführer getroffen wurde.");
+-- Japanese
+INSERT OR REPLACE INTO LocalizedText
+        (Tag,    Language,    Text)
+VALUES	("LOC_PEDIA_LEADERS_PAGE_LEADER_SAILOR_VALKRANA_CHAPTER_DETAILED_BODY",
+		"ja_JP",
+		"スペルブック[NEWLINE][ICON_Bullet][COLOR_FLOAT_CULTURE]スクライ[ENDCOLOR] - 対象リーダーの所属する都市を永久に公開する。1回の使用で異なる都市を対象とするか、その都市の上空にあるあなたの視認半径を拡大する。[NEWLINE][ICON_Bullet][COLOR_MEDIUM_GREEN]キネティックバリア[ENDCOLOR] - 自分のユニットに防御時に戦闘力ボーナス+10を付与する。5ターン持続し、スタックする。 [NEWLINE][ICON_Bullet][COLOR:72,39,111,255]ウェルオブソウルズ[ENDCOLOR] - 自分のスケルトンを20+最後にこの呪文を使用してから破壊したユニット数の2倍の量だけ回復する。 [NEWLINE][ICON_Bullet][COLOR:72,39,111,255]スキールポート[ENDCOLOR] - 対象リーダーの都市付近で敵対スケルトン2～3体を出現させる。指導者がいない場合はランダムな区画をターゲットとする。");
