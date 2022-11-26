6c5e754c6698408951a6a693c9aeb38e6c560d9c
Sailor Cat <52355344+ActualSailorCat@users.noreply.github.com>
2022-11-25 20:07:13 -0500

    v1.11 Critical MP Fix

---
+++ Valkrana v1.11/Core/Sailor_Valkrana_XP.sql
@@ -0,0 +1,4 @@
+-- // MomentIllustration
+INSERT INTO MomentIllustrations
+		(MomentIllustrationType,			MomentDataType,			GameDataType,				Texture)
+VALUES ('MOMENT_ILLUSTRATION_UNIQUE_UNIT',	'MOMENT_DATA_UNIT',		'UNIT_SAILOR_VALKRANA_UU',	'Moment_UniqueUnit_Sailor_Valkrana_UU.dds');
