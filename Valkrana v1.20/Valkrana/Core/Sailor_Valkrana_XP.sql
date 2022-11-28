e1d6e67a085d3d45d962e32b4ef9e8a2b7b7bd8f
Sailor Cat <52355344+ActualSailorCat@users.noreply.github.com>
2022-11-28 12:20:48 -0500

    v1.20

---
+++ Valkrana v1.20/Valkrana/Core/Sailor_Valkrana_XP.sql
@@ -0,0 +1,5 @@
+-- // MomentIllustrations
+INSERT INTO MomentIllustrations
+		(MomentIllustrationType,			MomentDataType,			GameDataType,				Texture)
+VALUES	('MOMENT_ILLUSTRATION_UNIQUE_UNIT',	'MOMENT_DATA_UNIT',		'UNIT_SAILOR_VALKRANA_UU',	'Moment_UniqueUnit_Sailor_Valkrana_UU.dds'),
+		('MOMENT_ILLUSTRATION_RELIGION',	'MOMENT_DATA_RELIGION', 'RELIGION_SAILOR_ILYANA',	'PM_FoundedReligion_Ilyana');
