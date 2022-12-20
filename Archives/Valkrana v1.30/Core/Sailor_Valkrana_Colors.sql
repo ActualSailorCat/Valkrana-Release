547806f03b12f766e9fd48f0fdf0b2440a834104
Sailor Cat <67406410+valkrana@users.noreply.github.com>
2022-12-19 23:26:43 -0500

    v1.31

---
+++ Archives/Valkrana v1.30/Core/Sailor_Valkrana_Colors.sql
@@ -0,0 +1,35 @@
+--///////////////////////////////////////////////////////
+-- Valkrana / Colors
+--///////////////////////////////////////////////////////
+INSERT INTO Colors -- Primary is background, secondary foreground. Purple 127,1,254,255; Pink 252,73,178,255
+        (Type,									Color)
+VALUES  ('COLOR_SAILOR_VALKRANA_PRIMARY',		'127,1,254,255'),
+		('COLOR_SAILOR_VALKRANA_SECONDARY',		'14,14,14,255'),
+		('COLOR_SAILOR_VALKRANA_PRIMARY2',		'252,73,178,255'),
+        ('COLOR_SAILOR_VALKRANA_SECONDARY2',	'14,14,14,255'),
+		('COLOR_SAILOR_VALKRANA_PRIMARY3',		'59,45,34,255'),
+        ('COLOR_SAILOR_VALKRANA_SECONDARY3',	'151,48,255,255'),
+		('COLOR_SAILOR_VALKRANA_PRIMARY4',		'213,161,7,255'),
+		('COLOR_SAILOR_VALKRANA_SECONDARY4',	'117,32,32,255');
+
+INSERT INTO PlayerColors (
+		Type,
+		Usage, 
+		PrimaryColor, 
+		SecondaryColor, 
+		Alt1PrimaryColor, 
+		Alt1SecondaryColor, 
+		Alt2PrimaryColor, 
+		Alt2SecondaryColor, 
+		Alt3PrimaryColor, 
+		Alt3SecondaryColor)
+VALUES	('LEADER_SAILOR_VALKRANA',
+		'Unique',
+		'COLOR_SAILOR_VALKRANA_PRIMARY2',
+		'COLOR_SAILOR_VALKRANA_SECONDARY2',
+		'COLOR_SAILOR_VALKRANA_SECONDARY3',
+		'COLOR_SAILOR_VALKRANA_SECONDARY',
+		'COLOR_STANDARD_WHITE_DK',
+		'COLOR_SAILOR_VALKRANA_SECONDARY3',
+		'COLOR_SAILOR_VALKRANA_PRIMARY4',
+		'COLOR_SAILOR_VALKRANA_SECONDARY4');
---
+++ Valkrana v1.31/Core/Sailor_Valkrana_ModSupport.sql
@@ -0,0 +1,21 @@
+-- 6T
+INSERT OR REPLACE INTO AiFavoredItems (ListType, Favored, Item, Value) SELECT 'SAILOR_VALKRANA_Techs', 1, 'TECH_PRINTING', 0
+WHERE EXISTS (SELECT CivicType FROM Civics WHERE CivicType = 'CIVIC_6T_CITIZENSHIP');
+
+-- Buddhas of Bamyan
+INSERT OR REPLACE INTO AiFavoredItems (ListType, Favored, Item, Value) SELECT 'SAILOR_VALKRANA_Buildings', 1, 'BUILDING_BAMYAN', 50
+WHERE EXISTS (SELECT BuildingType FROM Buildings WHERE BuildingType = 'BUILDING_BAMYAN');
+
+INSERT OR REPLACE INTO AiFavoredItems (ListType, Favored, Item, Value) SELECT 'SAILOR_VALKRANA_Civics', 1, PrereqTech, 50 
+FROM Buildings WHERE BuildingType = 'BUILDING_BAMYAN'
+AND EXISTS (SELECT BuildingType FROM Buildings WHERE BuildingType = 'BUILDING_BAMYAN');
+
+-- Restore Flirtatious & Curmudgeon
+INSERT OR REPLACE INTO ExclusiveAgendas (AgendaOne, AgendaTwo) SELECT 'AGENDA_SAILOR_VALKRANA', AgendaType
+FROM Agendas WHERE AgendaType IN ('AGENDA_SAILOR_FLIRTATIOUS', 'AGENDA_SAILOR_CURMUDGEON')
+AND EXISTS (SELECT AgendaType FROM Agendas WHERE AgendaType = 'AGENDA_SAILOR_FLIRTATIOUS');
+
+-- Prurient and Frigid
+INSERT OR REPLACE INTO ExclusiveAgendas (AgendaOne, AgendaTwo) SELECT 'AGENDA_SAILOR_VALKRANA', AgendaType
+FROM Agendas WHERE AgendaType IN ('AGENDA_SAILOR_FRIGID', 'AGENDA_SAILOR_PRURIENT')
+AND EXISTS (SELECT AgendaType FROM Agendas WHERE AgendaType = 'AGENDA_SAILOR_FRIGID');
